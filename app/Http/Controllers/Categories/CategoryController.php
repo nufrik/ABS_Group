039d3509d89bf99386d40e2851bb591d48e55234
nufrik <daniilonufriev30@gmail.com>
2023-05-26 15:03:18 +0300

    контроллер для категорий

---
+++ app/Http/Controllers/Categories/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Categories;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+
+
+}
