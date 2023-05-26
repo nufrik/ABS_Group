c1bb4911a4d112cc129ec80d81dbfcc9d3ab2548
nufrik <daniilonufriev30@gmail.com>
2023-05-26 15:03:05 +0300

    контроллер для книг

---
+++ app/Http/Controllers/Books/BookController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Books;
+
+use App\Http\Controllers\Controller;
+use App\Services\Book\BookServiceInterface;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    private BookServiceInterface $bookService;
+
+    public function __construct(BookServiceInterface $bookService)
+    {
+        $this->bookService = $bookService;
+    }
+
+    public function showBooks(string $slug)
+    {
+        $data = $this->bookService->getBooksByCategorySlug($slug);
+
+        return view('books.books', [
+            'books' => $data,
+        ]);
+    }
+
+}
