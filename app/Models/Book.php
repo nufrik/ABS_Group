05727c2ae4d8b578b8919c96d3f32d03cd0bea44
nufrik <daniilonufriev30@gmail.com>
2023-05-26 15:02:00 +0300

    модель для книг

---
+++ app/Models/Book.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Book extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(Author::class);
+    }
+}
