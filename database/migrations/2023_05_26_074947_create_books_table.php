1ecabff626cb3c8abcfbd1f2badb8c5dd47e473b
nufrik <daniilonufriev30@gmail.com>
2023-05-26 14:59:38 +0300

    создал таблицу книг

---
+++ database/migrations/2023_05_26_074947_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->string('slug')->unique();
+            $table->integer('year');
+            $table->text('description');
+            $table->string('cover');
+            $table->integer('category_id');
+            $table->integer('author_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
