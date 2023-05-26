b3bba562fcb93514c428a979e7b59657675ccec3
nufrik <daniilonufriev30@gmail.com>
2023-05-26 14:52:04 +0300

    забиндил BookServiceInterface и BookService, добавил бутстрап пагинатор

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\Book\BookService;
+use App\Services\Book\BookServiceInterface;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            BookServiceInterface::class,
+            BookService::class,
+        );
     }
 
     /**
@@ -19,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive();
     }
 }
