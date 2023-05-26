4e47317c527372064e87a81a4588342dc88378d2
nufrik <daniilonufriev30@gmail.com>
2023-05-26 14:52:43 +0300

    Сидер для авторов

---
+++ database/seeders/AuthorsSeeder.php
@@ -0,0 +1,267 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\Author;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+
+class AuthorsSeeder extends Seeder
+{
+    public function run()
+    {
+        Author::insert([
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+            [
+                'name' => 'Name' .  random_int(1,20) . ' Surname' .  random_int(1,20) . ' Patronymic' .  random_int(1,20),
+                'country' => 'Country ' . random_int(1,20),
+                'comment' => 'Comment ' . random_int(1,20),
+            ],
+        ]);
+    }
+}
+?>
