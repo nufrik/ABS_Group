c484aacc4ef1a8e5c93f9ec940f636b764fd6e12
nufrik <daniilonufriev30@gmail.com>
2023-05-28 23:53:42 +0300

    реализовал ресурс для категорий

---
+++ app/Http/Resources/Resources/CategoryForBookResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryForBookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            //    'books' => BookResource::collection($this->books),
+            //    'updeted_at' => $this->updeted_at,
+            //    'deleted_at' => $this->deleted_at,
+        ];
+    }
+}
